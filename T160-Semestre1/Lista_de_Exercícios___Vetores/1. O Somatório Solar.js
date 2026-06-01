46de641250b1d0efc4235a3c3145268c5ceecc08
4demarNet0 <euescritorui233@gmail.com>
2026-06-01 10:24:37 -0300

    Update 1. O Somatório Solar.js

--- T160-Semestre1/Lista_de_Exercícios___Vetores/1. O Somatório Solar.js
+++ T160-Semestre1/Lista_de_Exercícios___Vetores/1. O Somatório Solar.js
@@ -1,1 +1,27 @@
+// Você instalou um sistema de 4.5 kWp de energia solar e o inversor re-
+// gistra a geração diária em kWh. Desenvolva uma função calcularTotalGerado(geracaoDiaria)
+// que receba um vetor com a geração de 7 dias e retorne o total de energia gerada na semana.
+
+  // VARIÁVEIS
+
+let enerGerado = 4.5;
+let totalGerado = 0;
+
+  // FUNÇÕES
+    
+function calcularTotalGerado(geracaoDiaria){
+
+  for(let i = 0; i <= 7; i++){
+
+    totalGerado += geracaoDiaria;
+
+  }
+  
+  return totalGerado;
+  
+}
+
+        // OPERAÇÕES
+    
+console.log(calcularTotalGerado(enerGerado));
 
